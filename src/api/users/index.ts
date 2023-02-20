3ab617d96229d6382347da60c6f5a4610668cc67
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2023-02-20 12:02:15 +0100

    creating users api

--- src/api/users/index.ts
+++ src/api/users/index.ts
@@ -1,3 +1,46 @@
 import express from "express";
 import createError from "http-errors";
 import UserModel from "./model";
+import { createAccessToken } from "../../lib/auth/tools";
+import { JWTAuthMiddleware } from "../../lib/auth/jwtAuth";
+
+const usersRouter = express.Router();
+
+usersRouter.post("/register", async (req, res, next) => {
+  try {
+    const newUser = new UserModel(req.body);
+    const { _id } = await newUser.save();
+    const payload = { _id: newUser._id, name: newUser.name };
+    const accessToken = await createAccessToken(payload);
+    res.status(201).send({ accessToken, _id });
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.checkCredentials(email, password);
+    if (user) {
+      const payload = { _id: user._id, name: user.name };
+      const accessToken = await createAccessToken(payload);
+      res.send({ accessToken });
+    } else {
+      next(createError(401, "Credentials are not ok!"));
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+usersRouter.get("/", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const users = await UserModel.find();
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default usersRouter;
